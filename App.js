3a8981e2073d0295bca8d885ac72d456664bbd9e
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-06-28 17:37:54 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color: 'blue'}}>Hello World</Text>
+      <Text style={{color: 'green'}}>Hello World</Text>
+      <Text style={{color: 'yellow'}}>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
