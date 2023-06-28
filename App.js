80e8eedcf78e326fba0700acb68437ec190cbe68
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-06-29 01:53:21 +0530

    learned FlatList, TouchableOpacity

--- App.js
+++ App.js
@@ -1,38 +1,95 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, Touchable, TouchableOpacity } from 'react-native';
 
 
 export default function App() {
 
-  const [name,setName] = useState('Lakindu');
-  const [person,setPerson] = useState({name:'Lakindu',age:24});
-  
-  const clickHandler = () => {
-    setName('Lakindu Widuranga');
-    setPerson({name:'Lakindu Widuranga',age:25});
+  const [people,setPeople] = useState([
+    {name:'Stark',id:'8'},
+    {name:'Thor',id:'9'},
+    {name:'Hulk',id:'10'},
+    {name:'Captain',id:'11'},
+    {name:'Black Widow',id:'12'},
+    {name:'Hawkeye',id:'13'},
+    {name:'Spiderman',id:'14'},
+    {name:'Black Panther',id:'15'},
+    {name:'Doctor Strange',id:'16'},
+    {name:'Captain Marvel',id:'17'},
+  ]);
+
+  const [name,setName] = useState('')
+
+  const pressHandeler = (name) => {
+      setName(name);
+
+      setPeople((prevPeople)=>{
+        return prevPeople.filter(person => person.name != name)
+      })
   }
-
-
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text>my name is {person.name}. my age is {person.age} </Text>
-      <Text style={styles.buttonContainer}>
-        <Button title='Update State' onPress={clickHandler}></Button>
-      </Text>
+
+      {/* method 1 is FlatList. its used for large amount of data */}
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item)=>item.id}
+        data={people}
+        renderItem={({item})=>(
+          <TouchableOpacity onPress={() => pressHandeler(item.name, item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      
+      <Text style={styles.text}>{name}</Text>
+      {/* ScrollView is used for small amount of data */}
+      {/* <ScrollView>
+        {people.map(item => (
+            <View key={item.key}>
+              <Text style={styles.item}>{item.name}</Text>
+            </View>
+        ))}
+      </ScrollView> */}
+
     </View>
   );
 }
 
+/* Styles */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:40,
+    paddingHorizontal:20
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   ButtonContainer:{
     marginTop:20
+  },
+  input:{
+    borderWidth:1,
+    borderColor:'#777',
+    padding:8,
+    margin:10,
+    width:200,
+    borderRadius:10
+  },
+  item:{
+    marginTop:24,
+    padding:30,
+    backgroundColor:'black',
+    fontSize:24,
+    marginHorizontal:10,
+    color:'white',
+  },
+  text:{
+    marginTop:20,
+    fontSize:40,
+    color:'black',
+    textAlign:'center',
+    marginBottom:50
   }
 });
 
